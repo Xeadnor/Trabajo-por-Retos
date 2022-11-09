4cafb186a76e498fde73fbcfa92e8b5341bd98b1
Marta <m.torresdlc@gmail.com>
2022-11-09 22:04:20 +0100

    apariencia formulario y textos, logo principal a secundario con scroll, cambio imagen

--- JS/validacionJS.js
+++ JS/validacionJS.js
@@ -1,32 +1,60 @@
+//elementos formulario
 const form = document.getElementById("formulario");
-const boton = document.getElementById("boton")
+const boton = document.getElementById("boton");
+const rest = document.getElementById("reset"); 
+//maenjadores de eventos de elementos del formulario
+rest.addEventListener("click",resetear);
+form.addEventListener("keydown",env);
 boton.addEventListener("click", validar);
-const tarjeta = document.getElementById("tarjeta");
+
+//elementos para la comprobacion al enviar
+const divSubmit=document.getElementById("r10")
+const resultSubmit= document.getElementById("v10");
+
+
+//elementos para la validacion de nombre y apellidos
 const nombre = document.getElementById("nombre");
 const apellidos = document.getElementById("apellidos");
+//elementos para mostrar resultado validacion nombre y apellidos
+const resNom= document.getElementById("v1");
+const divNom = document.getElementById("r1");
+const resAp= document.getElementById("v2"); 
+
+
+//elementos para la validacion de email
+const mail = document.getElementById("mail");
+//elementos para mostrar resultado validacion de email
+const divE = document.getElementById("r2");
+const resM= document.getElementById("v3");
+
+
+//elementos para la validacion de la contrasenna
 const psw1 = document.getElementById("contrasena1");
 const psw2 = document.getElementById("contrasena2");
-const pais= document.getElementById("pais");
-const dir1= document.getElementById("direccion1");
-const dir2= document.getElementById("direccion2");
-const dir3= document.getElementById("direccion3");
-const dir4= document.getElementById("direccion4");
-const mail = document.getElementById("mail");
-const fecha = document.getElementById("fecha");
-const resNom= document.getElementById("v1");
-const divNom = document.getElementById("r1")
-const divE = document.getElementById("r2")
-const resAp= document.getElementById("v2"); 
-const resM= document.getElementById("v3");
-const divD = document.getElementById("r3")
-const divC = document.getElementById("r4")
-const resF= document.getElementById("v4");
+//elementos para mostrar resultado validacion de constrasenna
+const divC = document.getElementById("r4");
 const resC= document.getElementById("v5");
+
+
+//elementos para la validacion de la fecha nacimiento
+const fecha = document.getElementById("fecha");
 const ty = document.getElementById("ty");
 const t1 = document.getElementById("t1");
 const t2 = document.getElementById("t2");
 const t3 = document.getElementById("t3");
 const t4 = document.getElementById("t4");
+//elementos para mostrar resultado validacion fecha nacimiento
+const divD = document.getElementById("r3");
+const resF= document.getElementById("v4");
+
+
+//elementos para la validacion de la direccion
+const pais= document.getElementById("pais");
+const dir1= document.getElementById("direccion1");
+const dir2= document.getElementById("direccion2");
+const dir3= document.getElementById("direccion3");
+const dir4= document.getElementById("direccion4");
+//elementos para mostrar resultado validacion de direccion
 const divDir1 = document.getElementById("r5")
 const divDir3 = document.getElementById("r6")
 const divDir4 = document.getElementById("r7")
@@ -34,40 +62,50 @@ const divCard = document.getElementById("r8")
 const resDir1 = document.getElementById("v6");
 const resDir3 = document.getElementById("v7");
 const resDir4 = document.getElementById("v8");
+
+
+//elementos para validar la tarjeta
+const tarjeta = document.getElementById("tarjeta");
+const ayoC = document.getElementById("ayoCaducidad")
+const mesC = document.getElementById("mesCaducidad")
+const cardN = document.getElementById("cardNumber");
+const card = document.getElementsByName("tipotarjeta")
+const owner = document.getElementById("owner")
+const cvv = document.getElementById("cvv")
+dir1.addEventListener("keyup",comprobarDirPais);
+dir2.addEventListener("keyup",comprobarDirPais);
+dir3.addEventListener("keyup",comprobarDirPais);
+dir4.addEventListener("keyup",comprobarDirPais);
+pais.addEventListener("click",comprobarDirPais);
+//elementos para mostrar resultado de validaciones de la tarjeta
+const cn = document.getElementById("v9");
 const divO = document.getElementById("r11");
 const divCVV = document.getElementById("r12");
 const resO = document.getElementById("v11");
 const resCVV = document.getElementById("v12");
 const resCaducidad = document.getElementById("v13");
 const divCaducidad = document.getElementById("r13");
-const ayoC = document.getElementById("ayoCaducidad")
-const mesC = document.getElementById("mesCaducidad")
 
-const cn = document.getElementById("v9");
-const divSubmit=document.getElementById("r10")
-const resultSubmit= document.getElementById("v10");
-const cardN = document.getElementById("cardNumber");
-const card = document.getElementsByName("tipotarjeta")
 
-const owner = document.getElementById("owner")
-const cvv = document.getElementById("cvv")
 
-dir1.addEventListener("keyup",comprobarDirPais);
-dir2.addEventListener("keyup",comprobarDirPais);
-dir3.addEventListener("keyup",comprobarDirPais);
-dir4.addEventListener("keyup",comprobarDirPais);
-pais.addEventListener("click",comprobarDirPais);
+//Elementos del header para modificar al hacer scroll
 const header = document.getElementById("headerLogo");
 const m1 = document.getElementById("m1");
-const logo = document.getElementById("logo");
+const logo = document.getElementById("logo"); 
+const body = document.getElementById("body"); 
+const seta= document.getElementById("seta"); 
+const divLogo1= document.getElementById("logo1"); 
+const divLogo2= document.getElementById("logo2"); 
 document.addEventListener("scroll",headL);
-const rest = document.getElementById("reset");
-rest.addEventListener("click",resetear);
-const body = document.getElementById("body");
-form.addEventListener("keydown",env);
+
+
+
+//al enviar se coulte si estaba mostrado el mensaje de "El formulario se ha enviado correctamente"
 function env() {
     divSubmit.style.display="none";
 }
+
+//al resetear todos los campos de mensajes de validacion que se estuvieran mostrando se ocultan
 function resetear() {
     divCard.style.display="none"
     divDir4.style.display="none"
@@ -81,42 +119,52 @@ function resetear() {
     divSubmit.style.display="none";
 }
 
+//al hacer scroll
 function headL() {
-    console.log(window.innerWidth);
-    console.log(window.scrollY);
+
     if(window.innerWidth > 600){
-        if(window.scrollY > 100){
+        if(window.scrollY > 100){//desaparece el logo con las letras de frikisetas y aparece sólo la seta
             m1.style.display="none";
-            logo.style.marginLeft="-35.8vw";
-            logo.style.width="12vw"
-            logo.style.height="7vw"
-            header.style.height="6vw";
-        }else{
+            divLogo1.style.display="none";
+            divLogo2.style.display="inline";
+            divLogo2.style.marginLeft="0vw";
+            divLogo2.style.width="12vw"
+            divLogo2.style.height="7vw"
+            header.style.height="7vw";
+           
+        }else{//al volver arriba vuelve a aparecer el logo con las letras
             m1.style.display="flex";
-            logo.style.marginLeft="0vw";
-            logo.style.width="20vw"
-            logo.style.height="10vw"
+            divLogo1.style.display="inline";
+            divLogo2.style.display="none";
+            divLogo1.style.marginLeft="35vw";
+            divLogo1.style.width="20vw"
+            divLogo1.style.height="10vw"
             header.style.height="9vw";
         }
     }else if(window.innerWidth < 600){
         if(window.scrollY > 50){
             m1.style.display="none";
-            logo.style.marginLeft="-40vw";
-            logo.style.width="20vw"
-            logo.style.height="13vw"
-            header.style.height="11vw";
+            divLogo1.style.display="none";
+            divLogo2.style.display="inline";
+            divLogo2.style.marginLeft="15vw";
+            divLogo2.style.marginTop="2vw";
+            divLogo2.style.width="20vw"
+            divLogo2.style.height="13vw"
+            header.style.height="12vw";
         }else{
             m1.style.display="flex";
-            logo.style.marginLeft="0vw";
-            logo.style.width="28vw"
-            logo.style.height="16vw"
+            divLogo1.style.display="inline";
+            divLogo2.style.display="none";
+            divLogo1.style.marginLeft="38vw";
+            divLogo1.style.marginTop="2vw";
+            divLogo1.style.width="28vw"
+            divLogo1.style.height="16vw"
             header.style.height="13vw";
         }
     }
+} 
 
-    
-}
-
+//al enviar
 function validar(e){
     e.preventDefault();
 
@@ -263,7 +311,7 @@ function comprobarContrasenna(psw1, psw2){
         divC.style.display="none";
         return true;
     }else{
-        console.log("false")
+       
         if(!long.test(psw1.value)){
             divC.style.display="flex";
             resC.innerHTML= "La contraseña debe tener al menos 8 caracteres";
@@ -454,7 +502,6 @@ function comprobardatecard(ayo,mes) {
         divCaducidad.style.display="none";
         return true;
     }
-    
 }
 
 
