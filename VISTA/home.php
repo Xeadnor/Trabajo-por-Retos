d9996d4c7623f32f13e788b3ca4dce60c6fc1c58
Pablo Sainz <zapencio@gmail.com>
2022-12-30 01:01:05 +0100

    Carousel added

--- VISTA/home.php
+++ VISTA/home.php
@@ -211,6 +211,47 @@ if ( window.history.replaceState ) {
 }
 
  ?>
+
+<div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+  <!-- Indicators/dots -->
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active bg-danger"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="1" class="bg-danger"></button>
+    <button type="button" data-bs-target="#demo" data-bs-slide-to="2" class="bg-danger"></button>
+  </div>
+  
+  <!-- The slideshow/carousel  1420x400-->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="VISTA/IMAGENES/slide1.jpg" alt="slide 1" class="d-block" style="width:100%">
+    </div>
+    <div class="carousel-item">
+      <img src="VISTA/IMAGENES/slide2.png" alt="slide 2" class="d-block" style="width:100%">
+    </div>
+    <div class="carousel-item">
+      <img src="VISTA/IMAGENES/slide3.png" alt="slide 3" class="d-block" style="width:100%">
+    </div>
+  </div>
+  
+  <!-- Left and right controls/icons -->
+  
+  <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon bg-dark rounded-circle" id="carousel1"></span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+    <span class="carousel-control-next-icon bg-dark rounded-circle"></span>
+  </button>
+</div>
+
+
+
+
+
+
+
+
+
 </body>
 <script src="VISTA/js/bootstrap.bundle.min.js"></script>
   <script src="JS/home.js"></script>
