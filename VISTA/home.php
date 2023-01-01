29b464d55f85a2f2c1e26e023f4c6ba85b63a18a
Pablo Sainz <zapencio@gmail.com>
2023-01-01 23:39:59 +0100

    fixed footer

--- VISTA/home.php
+++ VISTA/home.php
@@ -16,12 +16,13 @@
     <title>Frikisetas</title>
     <link rel="stylesheet" href="style.css">
     <link href="VISTA/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
 </head>
 <body id="body">
 
 
-<div class="container-fluid justify-content-center" style="z-index:2;" id="divlogin">
-    <div class="row d-flex justify-content-center align-items-center" id="ti">
+<div class="container-fluid justify-content-center" style="z-index:3;" id="divlogin">
+    <div class="row d-flex justify-content-center align-items-center">
       <div class="col-12 col-md-8 col-lg-6 col-xl-4" id="ti">
         <div class="card bg-dark text-white" style="border-radius: 2rem;" id="ti">
           <div class="card-body p-3 text-center">
@@ -189,7 +190,7 @@ if(isset($_SESSION["errorLogado"])){
 
 function logar2(e) {
     document.onclick = function(e){
-        if(e.target.id !== 'divlogin' && e.target.id !== 'login' && e.target.id !== 'formlogin'
+        if(e.target.id !== 'login' && e.target.id !== 'formlogin'
         && e.target.id !== 'usuario'
         && e.target.id !== 'password'
         && e.target.id !== 'reenviador'
@@ -211,6 +212,12 @@ if ( window.history.replaceState ) {
 }
 
  ?>
+
+<div class="row justify-content-center col-12 m-0" style="z-index:1;" id="he">
+    <div class="col-12 d-flex justify-content-center bg-danger">
+    <p style="font-size:30px">POSIBLE TEXTO O FRASE GANCHO?</p>
+    </div>
+    </div>
 
 <div id="demo" class="carousel slide" data-bs-ride="carousel">
 
@@ -244,9 +251,157 @@ if ( window.history.replaceState ) {
   </button>
 </div>
 
+<div class="row justify-content-center col-12 m-0" style="z-index:1;" id="he">
+    <div class="col-12 d-flex justify-content-center bg-danger">
+    <p style="font-size:30px">BEST SELLERS? MAS VENDIDOS? RECOMENDADOS?</p>
+    </div>
+    </div>
 
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-white text-muted">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Mantente informado siguiendonos en nuestras redes sociales:</span>
+    </div>
+    <!-- Left -->
 
+    <!-- Right -->
+    <div>
+    <a
+        class="btn text-white btn-floating m-1"
+        style="background-color: #3b5998;"
+        href="#!"
+        role="button"
+        ><i class="fab fa-facebook-f"></i
+      ></a>
 
+      <!-- Twitter -->
+      <a
+        class="btn text-white btn-floating m-1"
+        style="background-color: #55acee;"
+        href="#!"
+        role="button"
+        ><i class="fab fa-twitter"></i
+      ></a>
+
+      <!-- Google -->
+      <a
+        class="btn text-white btn-floating m-1"
+        style="background-color: #dd4b39;"
+        href="#!"
+        role="button"
+        ><i class="fab fa-google"></i
+      ></a>
+
+      <!-- Instagram -->
+      <a
+        class="btn text-white btn-floating m-1"
+        style="background-color: #ac2bac;"
+        href="#!"
+        role="button"
+        ><i class="fab fa-instagram"></i
+      ></a>
+
+      <!-- Github -->
+      <a
+        class="btn text-white btn-floating m-1"
+        style="background-color: #333333;"
+        href="#!"
+        role="button"
+        ><i class="fab fa-github"></i
+      ></a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+          <i class='fas fa-tshirt me-3 text-secondary'></i> Frikisetas
+          </h6>
+          <p>
+            Posible texto?¿?¿??¿
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Products
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Camisetas</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Ofertas</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">?=?=?</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">?¿¿?¿?</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            links a algo?
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">?</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">?</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">?</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">?</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3 text-secondary"></i> Direccion?</p>
+          <p>
+            <i class="fas fa-envelope me-3 text-secondary"></i>
+            frikisetas@gmail.com
+          </p>
+          <p><i class="fas fa-phone me-3 text-secondary"></i> Telefono?</p>
+          <p><i class="fas fa-print me-3 text-secondary"></i> Que es este icono wtf</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.025);">
+    © 2023 Copyright:
+    <a class="text-reset fw-bold" href="#">HumanifyWebDesign</a>
+  </div>
+  <!-- Copyright -->
+</footer>
 
 
 
@@ -254,6 +409,7 @@ if ( window.history.replaceState ) {
 
 </body>
 <script src="VISTA/js/bootstrap.bundle.min.js"></script>
+<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
   <script src="JS/home.js"></script>
 </html>
 
